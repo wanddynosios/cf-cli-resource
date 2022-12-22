1846fdffeff0ae87d8819abd775e52967c43f4a3
wanddynosios <67467325+wanddynosios@users.noreply.github.com>
2022-12-22 10:13:32 +0100

    fix bad copy&paste

--- resource/commands/exec.sh
+++ resource/commands/exec.sh
@@ -5,12 +5,3 @@ logger::info "Executing $(logger::highlight "$command"): $exec"
 cf::target "$org" "$space"
 
 cf::cf $exec
-h061213@sandbox():/tmp$ cat exec.sh
-
-exec="$(get_option '.exec')"
-
-logger::info "Executing $(logger::highlight "$command"): $exec"
-
-cf::target "$org" "$space"
-
-cf::cf $exec
